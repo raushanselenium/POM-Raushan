08e1cbf5ae9b5c2e9d9a34c4e71b282f1ce56d60
Raushan Kumar <vicky.raushan@gmail.com>
2022-02-09 02:33:10 +0530

    Raushan first commit  in this project-> Added BranchRaushan Test case

---
+++ src/test/java/com/crm/qa/testcases/BranchRaushanTest.java
@@ -0,0 +1,13 @@
+package com.crm.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class BranchRaushanTest {
+	
+	@Test
+	public void branchRaushanTest()
+	{
+		System.out.println("Testing branch in git");
+	}
+
+}
